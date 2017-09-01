c07b2de101abfa6c3f4dcfb761449c9468a02af3
TropComplique <antoshchenko.b@gmail.com>
2017-09-01 20:12:14 +0300

    add more docs

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Image Classification Experiments on Caltech-256 dataset
-## 
+##
 
 | model | accuracy, % | top 5 accuracy, %| number of parameters |
-| --- | --- | --- | --- | 
+| --- | --- | --- | --- |
 | DenseNet-121    | 85 | 96 | 7,216,256 |
 | DenseNet-121 with trained ternary quantization | 73 | 91 | ~7.2M 2-bit, ~79% zeros |
 | DenseNet-121 with binary weights | 41 | 67 | ~7.2M 1-bit |
@@ -10,11 +10,12 @@
 | ResNet-18 | 81 | 94 | 11,307,840 |
 | ResNet-18 with trained ternary quantization | 52 | 78 | ~11M 2-bit, ~86% zeros |
 | ResNet-18 with stochastic depth | 70 | 89 | 11,307,840 |
-| SqueezeNet v1.1 | 66 | 85 | 853,824 | 
+| SqueezeNet v1.1 | 66 | 85 | 853,824 |
 | SqueezeNet v1.1 with entropy regularization | 66 | 86 | 853,824 |
 | Delegating classifiers | 77 | 93 | - |
 
 ## Trained ternary quantization
+Trained ternary quantization [2] is a method to reduce size of a neural network. It replaces all weights in a convolutional layer with +1, 0, -1 and two float scaling factors (one for +1 and one for -1).
 
 ## Binary-Weights-Networks
 
@@ -25,6 +26,8 @@
 ## Delegating classifiers
 
 ## SmoothGrad
+SmoothGrad is a method of computing nice sensitivity maps. Sensitivity maps show which image pixels influence class predictions. They require to compute gradients with respect to an input image. Here are a few examples:
+![sensitivity maps with smoothgrad](sensitivity_maps.png)
 
 ## Notes
 * Cyclical Cosine Annealing
--- training_utils/diagnostic_tools.py
+++ training_utils/diagnostic_tools.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
 
-"""Tools for diagnostic if a learned system.
+"""Tools for diagnostic of a learned model.
 
 Arguments:
-    
+
     true: a numpy array of shape (n_samples,) of type int
         with integers in range 0..(n_classes - 1).
-    
+
     pred: a numpy array of shape (n_samples, n_classes) of type float,
         represents probabilities.
-        
+
     decode: a dict that maps a class index to human readable format.
 """
 
@@ -44,7 +44,7 @@ def most_inaccurate_k_classes(per_class_acc, k, decode):
     most = per_class_acc.argsort()[:k]
     for i in most:
         print(decode[i], per_class_acc[i])
-    
+
 
 def entropy(pred):
 
@@ -60,13 +60,13 @@ def model_calibration(true, pred, n_bins=10):
     On Calibration of Modern Neural Networks,
     https://arxiv.org/abs/1706.04599
     """
-    
+
     hits = np.equal(pred.argmax(1), true)
-    
+
     fraction_of_positives, mean_predicted_value = calibration_curve(
         hits, pred.max(1), n_bins=n_bins
     )
-    
+
     plt.plot(mean_predicted_value, fraction_of_positives, '-ok');
     plt.plot([0.0, 1.0], [0.0, 1.0], '--');
     plt.xlim([0.0, 1.0]);
@@ -85,24 +85,24 @@ def count_params(model):
 
 
 def most_confused_classes(val_true, val_pred, decode, min_n_confusions):
-    
+
     conf_mat = confusion_matrix(val_true, val_pred.argmax(1))
-    
+
     # not interested in correct predictions
     conf_mat -= np.diag(conf_mat.diagonal())
-    
+
     # confusion(class A -> class B) + confusion(class B -> class A)
     conf_mat += conf_mat.T
-    
+
     confused_pairs = np.where(np.triu(conf_mat) >= min_n_confusions)
     confused_pairs = [(k, confused_pairs[1][i]) for i, k in enumerate(confused_pairs[0])]
     confused_pairs = [(decode[i], decode[j]) for i, j in confused_pairs]
-    
+
     return confused_pairs
 
 
 def show_errors(erroneous_samples, erroneous_predictions, erroneous_targets, decode):
-    
+
     n_errors = len(erroneous_targets)
     # choose 30 random erroneous predictions
     to_show = np.random.choice(np.arange(0, n_errors), size=30, replace=False)
@@ -111,16 +111,16 @@ def show_errors(erroneous_samples, erroneous_predictions, erroneous_targets, dec
     pictures_predictions = erroneous_predictions.argsort(1)[:, -5:][to_show]
     pictures_probs = np.sort(erroneous_predictions, 1)[:, -5:][to_show]
     pictures_true = erroneous_targets[to_show]
-    
+
     # values for pretrained pytorch models
     mean = np.array([0.485, 0.456, 0.406], dtype='float32')
     std = np.array([0.229, 0.224, 0.225], dtype='float32')
-    
+
     pictures = np.transpose(pictures, axes=(0, 2, 3, 1))
     # reverse normalization
     pictures *= std
     pictures += mean
-    
+
     # show pictures, predicted classes, probabilities, and true classes
     _, axes = plt.subplots(nrows=6, ncols=5, figsize=(14, 19))
     axes = axes.flatten()
@@ -136,5 +136,5 @@ def show_errors(erroneous_samples, erroneous_predictions, erroneous_targets, dec
             'true: ' + decode[pictures_true[i]]
 
         axes[i].set_title(title);
-    
+
     plt.tight_layout()
